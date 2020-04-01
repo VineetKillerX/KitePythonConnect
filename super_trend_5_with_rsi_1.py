533e987c1b3f3f5a0e8b206a995d66c2b1687ae2
Vinit Ludhani <vineetludhani123@gmail.com>
2020-04-01 10:47:48 +0530

    Update super_trend_5_with_rsi_1.py

--- super_trend_5_with_rsi_1.py
+++ super_trend_5_with_rsi_1.py
@@ -18,6 +18,9 @@ file_name="holdings_log.csv"
 import pytz
 tz = pytz.timezone('Asia/Kolkata')
 holding =""
+rs1_trend_log="rsi_trend.csv"
+
+
 def generate_url():
 	global kite
 	kite = KiteConnect(api_key,api_secret)
@@ -30,7 +33,7 @@ def run(request_token):
 	access_token = data["access_token"]
 	kite.set_access_token(access_token)
 	jobs = []
-	tokens = ['1510401','408065','633601','1270529','779521','340481','738561','348929','877057']
+	tokens = ['1510401']
 	for instrument_token in tokens:
 		process = multiprocessing.Process(target=trade,args=(instrument_token,))
 		jobs.append(process)
@@ -38,6 +41,25 @@ def run(request_token):
 		j.start()
 	for j in jobs:
 		j.join()
+
+
+
+
+
+
+
+def run(request_token,token):
+  global kite
+  data = kite.generate_session(request_token,api_secret)
+  access_token = data["access_token"]
+  kite.set_access_token(access_token)
+  trade(token)
+
+
+
+
+
+
 
 
 
@@ -49,14 +71,14 @@ def trade(token):
   stop_loss = 0.01
   last_price = 0.00
   last_min=-1
-  datetime_obj_hour_fwd=datetime.now()+timedelta(hours=1)
+  datetime_obj_hour_fwd=getDateTime()+timedelta(hours=1)
   from_date=str(datetime_obj_hour_fwd-timedelta(days=14)).split(" ")[0]
   to_date=str(datetime_obj_hour_fwd).split(" ")[0]
   while True:
-    datetime_obj=datetime.now(tz)
+    datetime_obj=getDateTime()
     min=int(str(datetime_obj).split(".")[0].split(":")[1])
     if(min%5==0 and (last_min==-1 or (min!=last_min and last_min!=-1))):
-      if(last_min==-1 or datetime_obj_hour_fwd<=datetime_obj):
+      if(last_min==-1 or (datetime_obj_hour_fwd<=datetime_obj and min==15)):
         historical_data_rsi = kite.historical_data(instrument_token=token,from_date=from_date,to_date=to_date,interval='hour')
         historical_data_rsi_df=pd.DataFrame(historical_data_rsi)
         df_rsi = indicators.RSI(historical_data_rsi_df,'close',14)
@@ -68,6 +90,7 @@ def trade(token):
       df = indicators.SuperTrend(df,7,3,['open','high','low','close'])
       latest_data = df.tail(1)
       signal = str(latest_data['STX_7_3'].get(latest_data['STX_7_3'].index.start))
+      write_log(str(rsi)+","+str(signal)+","+str(datetime_obj)+"\n",rs1_trend_log)
       if (datetime_obj.hour == 15 and datetime_obj.minute>15):
         profit = 0.01
         stop_loss = 0.005
@@ -75,21 +98,30 @@ def trade(token):
         if(signal=='down' and holding!='down' and rsi<50):
           quote = kite.quote(token)
           price = quote[token]['last_price']
-          write_log("Sell"+","+str(token)+","+str(price)+",supertrend,"+str(datetime.now(tz))+"\n")
+          write_log("Sell"+","+str(token)+","+str(price)+",supertrend,"+str(getDateTime())+"\n")
           holding = 'down'
           last_price = price
         elif (signal == 'up' and holding != 'up' and rsi>50):
           quote = kite.quote(token)
           price = quote[token]['last_price']
-          write_log("Buy"+","+str(token)+","+str(price)+",supertrend,"+str(datetime.now(tz))+"\n")
+          write_log("Buy"+","+str(token)+","+str(price)+",supertrend,"+str(getDateTime())+"\n")
           holding = 'up'
           last_price = price
     else:
-	  stoper(token,last_price)
+      stoper(token,last_price)
     time.sleep(1)		
-	
-			
-	
+
+
+
+def getDateTime():
+  datetime_obj_hour_fwd=datetime.now(tz)
+  return datetime_obj_hour_fwd
+
+
+
+
+
+
 def stoper(token,last_price):
   quote = kite.quote(token)
   global holding
@@ -99,28 +131,33 @@ def stoper(token,last_price):
     temp_loss = (last_price-price)/last_price
     if(temp_profit>=profit):
       holding = ''
-      write_log("Sell"+","+str(token)+","+str(price)+",profit,"+str(datetime.now(tz))+"\n")
+      write_log("Sell"+","+str(token)+","+str(price)+",profit,"+str(getDateTime())+"\n")
     elif(temp_loss>stop_loss):
       holding = ''
-      write_log("Sell"+","+str(token)+","+str(price)+",stoploss,"+str(datetime.now(tz))+"\n")	
+      write_log("Sell"+","+str(token)+","+str(price)+",stoploss,"+str(getDateTime())+"\n")	
     if (datetime_obj.hour == 15 and datetime_obj.minute>28):
       holding = ''
-      write_log("Sell"+","+str(token)+","+str(price)+",market_close,"+str(datetime.now(tz))+"\n")
+      write_log("Sell"+","+str(token)+","+str(price)+",market_close,"+str(getDateTime())+"\n")
   elif (holding=='down'):
     temp_profit = (last_price-price)/last_price
     temp_loss = (price-last_price)/last_price
     if(temp_profit>=profit):
       holding = ''
-      write_log("Buy"+","+str(token)+","+str(price)+",profit,"+str(datetime.now(tz))+"\n")
+      write_log("Buy"+","+str(token)+","+str(price)+",profit,"+str(getDateTime())+"\n")
     elif(temp_loss>stop_loss):
       holding = ''
-      write_log("Buy"+","+str(token)+","+str(price)+",stoploss,"+str(datetime.now(tz))+"\n")
+      write_log("Buy"+","+str(token)+","+str(price)+",stoploss,"+str(getDateTime())+"\n")
     if (datetime_obj.hour == 15 and datetime_obj.minute>28):
       holding = ''
-      write_log("Buy"+","+str(token)+","+str(price)+",market_close,"+str(datetime.now(tz))+"\n")
+      write_log("Buy"+","+str(token)+","+str(price)+",market_close,"+str(getDateTime())+"\n")
+
+
+
+
+
 			
-def write_log(log):
-	f=open(file_name,'a')	
-	f.write(log)	
-	f.close()	
-				
+def write_log(log,file_name=file_name):
+  f=open(file_name,'a')	
+  f.write(log)	
+  f.close()	
+  				
