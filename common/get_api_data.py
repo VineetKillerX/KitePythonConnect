1ad366edd470d6e99566424f2945f34628bdac0d
Shivam.Gupta <Shivam.Gupta@BELPHP0111.FRACTAL.COM>
2020-04-04 01:49:28 +0530

    Added get data common funtionality

---
+++ common/get_api_data.py
@@ -0,0 +1,24 @@
+from common.create_kite_session import *
+kite=get_session()
+def get_data(token,from_date,to_date,interval,historical_data,counter=0):
+    try:
+        return kite.historical_data(instrument_token=token,from_date=from_date,to_date=to_date,interval=interval)
+    except:
+        if(counter<=3):
+            print("get_data exception ",counter)
+            return get_data(type_of_data,token,from_date,to_date,interval,historical_data,counter+1)
+        else:
+            print("returning old history_df ")
+            return historical_data
+
+
+def get_price(token,price,counter=0):
+    try:
+        return kite.quote(token)[token]['last_price']
+    except:
+        if(counter<=3):
+            print("get_price exception ",counter)
+            return get_price(token,price,counter+1)
+        else:
+            print("returning old price ",price)
+            return price
